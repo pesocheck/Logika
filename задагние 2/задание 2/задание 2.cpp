765a662827701fd2fa9710ee87f806c0759d805e
Alexey <aleksey222.makarov@yandex.ru>
2024-09-12 19:56:54 +0300

    добавил второе задание

---
+++ задагние 2/задание 2/задание 2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <windows.h>
+
+
+
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int size;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+
+    int* arr = new int[size];
+    srand(time(NULL));
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;
+    }
+    printf("Полученный массив: ");
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i] );
+    }
+    printf("\n");
+    delete[] arr;
+    return 0;
+}
