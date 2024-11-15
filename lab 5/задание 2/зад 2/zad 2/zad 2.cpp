af9eeec86421bf38221ad742a807efebcea5aed5
Alexey <aleksey222.makarov@yandex.ru>
2024-11-15 15:41:55 +0300

    Второе задание 5 лаб и 8 лаб

---
+++ lab 5/задание 2/зад 2/zad 2/zad 2.cpp
@@ -0,0 +1,113 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n = 5, ** G = NULL, ** I = NULL, size = 0, * deg = NULL;
+
+    printf("Введите количество вершин: ");
+    scanf("%d", &n);
+
+    deg = (int*)malloc(n * sizeof(int));
+    G = (int**)malloc(n * sizeof(int*));
+
+    for (int i = 0; i < n; i++) {
+        deg[i] = 0;
+        G[i] = (int*)malloc(n * sizeof(int));
+    }
+
+    // Инициализация матрицы смежности
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            G[i][j] = 0;
+
+    // Генерация случайного графа
+    srand(time(NULL)); // Инициализация генератора случайных чисел
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) { // j начинается с i + 1, чтобы избежать дублирования
+            G[i][j] = rand() % 2;
+            G[j][i] = G[i][j]; // Неориентированный граф
+        }
+    }
+
+    // Печать матрицы смежности
+    printf("Матрица смежности:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", G[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Подсчет размера графа и степени вершин
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) { // j начинается с i + 1, чтобы избежать дублирования
+            size += G[i][j];
+        }
+    }
+
+    printf("Размер графа = %d \n", size);
+
+    // Подсчет степени вершин
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            deg[i] += G[i][j];
+        }
+        printf("Deg[%d]: %d\n", i, deg[i]);
+    }
+
+    // Построение матрицы инцидентности
+    int m = size; // Количество рёбер
+    I = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        I[i] = (int*)malloc(m * sizeof(int));
+        for (int j = 0; j < m; j++) {
+            I[i][j] = 0; // Инициализация матрицы инцидентности
+        }
+    }
+
+    // Заполнение матрицы инцидентности
+    int edgeIndex = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (G[i][j] == 1) {
+                I[i][edgeIndex] = 1; // Вершина i инцидентна ребру
+                I[j][edgeIndex] = 1; // Вершина j инцидентна ребру
+                edgeIndex++;
+            }
+        }
+    }
+
+    // Печать матрицы инцидентности
+    printf("Матрица инцидентности:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", I[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Определение изолированных, концевых и доминирующих вершин
+    for (int i = 0; i < n; i++) {
+        if (deg[i] == 0)
+            printf("Вершина %d - изолированная\n", i);
+        else if (deg[i] == 1)
+            printf("Вершина %d - концевой\n", i);
+        else if (deg[i] == n - 1)
+            printf("Вершина %d - доминирующая\n", i);
+    }
+
+    // Освобождение памяти
+    for (int i = 0; i < n; i++) {
+        free(G[i]);
+        free(I[i]);
+    }
+    free(G);
+    free(I);
+    free(deg);
+
+    return 0;
+}
---
+++ lab 8/lab 8/lab 8.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include<queue>
+using namespace std;
+int** createG(int size) {
+    int** G = NULL;
+    G = (int**)malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++) {
+        G[i] = (int*)malloc(size * sizeof(int));
+    }
+    for (int i = 0; i < size; i++) {
+        for (int j = i; j < size; j++) {
+            G[i][j] = rand() % 2;
+            if (i == j) G[i][j] = 0;
+            G[j][i] = G[i][j];
+        }
+    }
+    return G;
+}
+
+void printG(int** G, int size)
+{
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d ", G[i][j]);
+        }
+        printf("\n");
+    }
+    return;
+}
+//int **G=NULL, size=5, vis[5]={0};
+void BFS(int** G, int size, int* vis, int s) {
+    queue <int>  q;
+    q.push(s);
+    vis[s] = 1;
+    while (!q.empty()) {
+        s = q.front();
+        printf("%d ", s);
+        q.pop();
+        for (int i = 0; i < size; i++) {
+            if (G[s][i] == 1 && vis[i] == 0) {
+                q.push(i);
+                vis[i] = 1;
+            }
+        }
+    }
+
+}
+
+
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int** G = NULL;
+    int* vis = NULL;
+    int size = 5;
+    int s = 1;
+    printf("Введите кол-во вершин: ");
+    scanf_s("%d", &size);
+    G = createG(size);
+    printG(G, size);
+    printf("Введите начальную вершину: ");
+
+    vis = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) vis[i] = 0;
+
+
+    scanf_s("%d", &s);
+    printf("Обход графа:\n");
+    BFS(G, size, vis, s);
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
